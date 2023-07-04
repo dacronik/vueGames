413134973574206da139a012b578e93971678590
dacronik <dacronik@gmail.com>
2023-07-03 21:34:09 -0400

    agregando pagina notFound

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 //import Opiniones from '@/views/Opiniones'
+import NotFound from '@/views/NotFound.vue'
 
 
 
@@ -22,13 +23,17 @@ const routes = [
     component: () => import(/* webpackChunkName: "Opiniones" */ '@/views/Administracion.vue')
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path:'/:pathMatch(.*)*',
+    component: NotFound,
   }
+  // {
+  //   path: '/about',
+  //   name: 'about',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  // }
 ]
 
 const router = createRouter({
---
+++ src/views/NotFound.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+        <div class="error">
+            <h1>Error 404</h1>
+            <h1>Page NotFound</h1>
+            <router-link to="/"><button>Regresar</button></router-link>
+            <router-view/>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'component-name',
+    // props: {},
+    data: function(){
+        return {}
+    },
+    // computed: {},
+    //methods: {}
+    // watch: {},
+    // components: {},
+    // mixins: [],
+    // filters: {},
+    // -- Lifecycle Methods
+    // -- End Lifecycle Methods
+}
+</script>
+
+<style scoped>
+    .error{
+        width: 100%;
+        height: 94vh;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        background-image: url("@/assets/img/error.jpg");
+        background-size: cover;
+        background-position: center;
+    }
+    .error h1{
+        font-size: 4rem;
+        font-weight: 700;
+        color: black;
+    }
+    .error button{
+        background: orangered;
+        border: none;
+        width: 155px;
+        padding: 20px;
+    }
+</style>
